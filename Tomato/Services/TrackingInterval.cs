8cc6edfcec5e7044fde4d365b7e1f757928ad180
Alex <hitech2009@gmail.com>
2022-10-07 18:06:07 +0300

    - stylecop

--- Tomato/Services/TrackingInterval.cs
+++ Tomato/Services/TrackingInterval.cs
@@ -4,22 +4,21 @@
 
     public struct TrackingInterval
     {
+        public static TrackingInterval WorkingTrack = new TrackingInterval(false, false, TimeSpan.FromMinutes(WorkingIntervalMins));
+        public static TrackingInterval RestTrack = new TrackingInterval(true, false, TimeSpan.FromMinutes(RestIntervalMins));
+        public static TrackingInterval LongRestTrack = new TrackingInterval(false, true, TimeSpan.FromMinutes(LongRestIntervalMins));
+
         private const string Zero = "00";
 
         private const int WorkingIntervalMins = 25;
         private const int RestIntervalMins = 5;
         private const int LongRestIntervalMins = 15;
 
-        public static TrackingInterval EmptyTrack = new TrackingInterval(false, false, TimeSpan.FromMinutes(0));
-        public static TrackingInterval WorkingTrack = new TrackingInterval(false, false, TimeSpan.FromMinutes(WorkingIntervalMins));
-        public static TrackingInterval RestTrack = new TrackingInterval(true, false, TimeSpan.FromMinutes(RestIntervalMins));
-        public static TrackingInterval LongRestTrack = new TrackingInterval(false, true, TimeSpan.FromMinutes(LongRestIntervalMins));
-
         private TrackingInterval(bool isRest, bool isLongRest, TimeSpan interval)
         {
             IsRestTrack = isRest;
             IsLongRestTrack = isLongRest;
-            IsWorkingTrack = !(IsRestTrack || IsLongRestTrack); ;
+            IsWorkingTrack = !(IsRestTrack || IsLongRestTrack);
             Interval = interval;
             Minutes = Zero;
             Seconds = Zero;
